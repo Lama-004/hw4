2ad1a021657b998f70ff8433a76987e009373b36
Lama-004 <161324856+Lama-004@users.noreply.github.com>
2026-05-02 11:34:49 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,55 @@
+function validateForm() {
+    var firstName = document.getElementById("first").value;
+    var lastName = document.getElementById("last").value;
+    var email = document.getElementById("email").value;
+
+    if (firstName == ""|| lastName == "" || email == "") {
+        alert("Please fill out all required fields");
+        return false;
+    }
+}
+
+
+
+function toggleDescription(id) {
+var descElement = document.getElementById(id);
+
+    if (descElement.style.display == "none") {
+        descElement.style.display = "block";
+    } else {
+        descElement.style.display = "block";
+        descElement.style.display = "none";
+    }
+}
+
+
+
+var quote = document.getElementById("quote");
+
+function onMouseOver() {
+quote.style.backgroundColor = "blue";
+}
+
+function onMouseOut() {
+    quote.style.backgroundColor = "";
+    quote.style.fontStyle = "italic";
+}
+
+if (quote) {
+    quote.addEventListener("mouseover", onMouseOver);
+    quote.addEventListener("mouseout", onMouseOut);
+}
+
+
+
+window.onload = function() {
+    var footer = document.getElementById("footer");
+
+    if (footer) {
+        var date = Date();
+        var message = document.createTextNode("Page loaded on: " + date);
+        var paragraph = document.createElement("p");
+        paragraph.appendChild(message);
+        footer.appendChild(paragraph);
+    }
+};
